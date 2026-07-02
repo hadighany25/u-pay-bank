f538103d9836045e87f75bbcc02005718dee0e49
hadighany25 <hadighany25@gmail.com>
2026-07-02 21:00:11 +0700

    Merchant

--- U-Pay Backend/routes/merchantRoutes.js
+++ U-Pay Backend/routes/merchantRoutes.js
@@ -1,21 +1,17 @@
 const express = require("express");
 const router = express.Router();
 
-// ១. Import Controllers
 const merchantController = require("../controllers/merchantController");
-
-// ២. Import Middleware (ត្រូវប្រាកដថាឈ្មោះនេះមានក្នុង authMiddleware.js)
 const { verifyUser } = require("../middleware/authMiddleware");
 
-// ៣. កំណត់ Routes (ត្រូវប្រើ verifyUser ទាំងអស់ដើម្បីឱ្យមាន req.user)
 router.post("/create", verifyUser, merchantController.createMerchant);
 router.get("/my-merchants", verifyUser, merchantController.getMyMerchants);
 
-// 🔥 កែត្រង់នេះ៖ ប្តូរពី authMiddleware មកប្រើ verifyUser វិញ
+// ត្រង់នេះ៖ ប្រសិនបើ merchantController.getMerchantTransactions ជា undefined វានឹង Error
 router.get(
-  "/revenue/:merchantId",
+  "/transactions/:merchantId",
   verifyUser,
-  merchantController.getMerchantRevenue,
+  merchantController.getMerchantTransactions,
 );
 
 module.exports = router;
